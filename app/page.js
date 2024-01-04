e2bb9a048e643faab7ff80d8ef6a2aacdd9923f7
Kaushal Rai <kaushalraiskt111@gmail.com>
2024-01-04 12:42:19 +0530

    Font optimization

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({ subsets: ["latin"], weight: "500" });
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Font optimizations</h1>
+      <main className={roboto.className}>Hello Google Font</main>
+    </main>
+  );
+}
